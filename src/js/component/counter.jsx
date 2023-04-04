b4d3c677c6d53567817c0be5deee80a689bdff62
Manuel Perez <manuelperez497078@gmail.com>
2023-04-03 21:46:47 -0400

    se sigue trabajando en el boton stop

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -1,50 +1,100 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import React from "react";
 
-
 //create your first component
-const SecondsCounter = (props) => {
+const SecondsCounter = () => {
+  const [counter, setCounter] = useState(0);
+  const [input, setInput] = useState("");
+  const [status, setStatus] = useState(true);
 
-	const [counter, setCounter] = useState(0);
-	const [input, setInput] = useState('')
+  function startCountdown({ target }) {
+    setInput(target.value);
+  }
 
-	function startCountdown({target}) {
-		setInput(target.value)
-    }
+  function submit(e) {
+    e.preventDefault();
+    setCounter((counter) => (counter = parseInt(input)));
+    setInput((input) => (input = ""));
+  }
 
-	function submit(e){
-		e.preventDefault()
-		console.log(input)
-		setCounter((counter)=> counter = input)
+  useEffect(()=>{
+	startCounter()
+  },[])
 
+  function startCounter() {
+    if (status === true) {
+      setInterval(() => {
+        setCounter((counter) => counter + 1);
+      }, 1000);
+    } else if (status === false){
+		clearInterval()
 	}
-
-	useEffect(() => {
-	  const interval = setInterval(() => {
-		setCounter((counter) => counter + 1);
-	  }, 1000);
-  
-	  return interval
-
-	}, []);
-
-	return (
-		<div>
-			<div id="counter" >
-				<i className="fa-regular fa-clock fa-5x "></i>
-				<p id="numbers">{counter}</p>
-			</div>
-			<form onSubmit={submit} id="setCountdown">
-				<div>
-					<label htmlFor="setCountdown" className="form-label">Set a countdown start</label>
-					<input type="text" value={input} onChange={startCountdown} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-				</div>
-				<button type="submit" className="btn btn-primary">Start Countdown</button>
-			</form>
-		</div>
-	);
-}
+  }
 
 
+	const stop = () => {
+		setStatus(false)
+		console.log("no para esta we..."+ status)
+	};
+
+
+
+
+
+
+
+  //   useEffect(() => {
+  //     var interval = setInterval(() => {
+  //       setCounter((counter) => counter + 1);
+  //     }, 1000);
+
+  //     return interval;
+  //   }, []);
+
+  //   const stop = () => {
+  // 	clearInterval(interval)
+  // 	console.log("paraaa")
+  //   };
+
+  const reset = () => {
+    setCounter(0);
+  };
+
+  return (
+    <div>
+      <div id="counter">
+        <i className="fa-regular fa-clock fa-5x "></i>
+        <p id="numbers">{counter}</p>
+      </div>
+      <form onSubmit={submit} id="setCountdown">
+        <div>
+          <label htmlFor="setCountdown" className="form-label">
+            Set a countdown start
+          </label>
+          <input
+            type="text"
+            value={input}
+            onChange={startCountdown}
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Start Countdown
+        </button>
+      </form>
+      <div className="controles d-flex justify-content-center gap-3">
+        <button className="btn btn-primary" onClick={stop}>
+          stop
+        </button>
+        <button className="btn btn-primary">Iniciar</button>
+        <button className="btn btn-primary" onClick={reset}>
+          reset
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default SecondsCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -7,24 +7,11 @@ import "../styles/index.css";
 
 //import your own components
 import SecondsCounter from "./component/counter.jsx";
-import SetCountdown from "./component/setCountdown.jsx";
 
 //render your react application
-
-var counter = 0
-
-
 ReactDOM.render(
-    <React.Fragment>
-        <SecondsCounter id="counter" seconds={counter}/>
-        {/* <SetCountdown/> */}
-    </React.Fragment>
-
-,document.querySelector("#app"));
-
-
-
-
-
-
-
+  <React.Fragment>
+    <SecondsCounter id="counter" />
+  </React.Fragment>,
+  document.querySelector("#app")
+);
